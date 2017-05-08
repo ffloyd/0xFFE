8210518a771f5ef77f98243c6db4637bf411629e
Roman Kolesnev <rvkolesnev@gmail.com>
2017-05-08 11:32:12 +0300

    Better elisp editing (evil-lispy, rainbow-delimeters)

--- init.el
+++ init.el
@@ -58,32 +58,31 @@
   (define-prefix-command 'ffe-ui-map)
 
   (general-define-key :keymaps 'ffe-buffers-map
-		      "n" 'next-buffer
-		      "p" 'previous-buffer)
+          "n" 'next-buffer
+          "p" 'previous-buffer)
 
   (general-define-key :keymaps 'ffe-apps-map
-		      "d" 'dired
-		      "u" 'undo-tree-visualize)
+          "d" 'dired
+          "u" 'undo-tree-visualize)
 
   (general-define-key :keymaps 'ffe-ui-map
-		      "F" 'toggle-frame-fullscreen
-		      "f" 'toggle-frame-maximized)
+          "F" 'toggle-frame-fullscreen
+          "f" 'toggle-frame-maximized)
 
   ;; Main SPC-keymap
   (define-prefix-command 'ffe-spc-map)
   (general-define-key :keymaps 'ffe-spc-map
-		      "a" '(ffe-apps-map :which-key "apps")
-		      "b" '(ffe-buffers-map :which-key "buffers")
-		      "f" '(ffe-files-map :which-key "files")
-		      "p" '(ffe-project-map :which-key "project")
-		      "s" '(ffe-search-map :which-key "search")
-		      "t" '(ffe-toggles-map :which-key "toggles")
-		      "w" '(evil-window-map :which-key "windows")
-		      "U" '(ffe-ui-map :which-key "UI"))
+          "a" '(ffe-apps-map :which-key "apps")
+          "b" '(ffe-buffers-map :which-key "buffers")
+          "f" '(ffe-files-map :which-key "files")
+          "p" '(ffe-project-map :which-key "project")
+          "s" '(ffe-search-map :which-key "search")
+          "t" '(ffe-toggles-map :which-key "toggles")
+          "w" '(evil-window-map :which-key "windows")
+          "U" '(ffe-ui-map :which-key "UI"))
 
   ;; now attach SPC-driven global keymap
-  (general-mmap "SPC" 'ffe-spc-map)
-  )
+  (general-mmap "SPC" 'ffe-spc-map))
 
 ;;
 ;; Color themes (solarized)
@@ -96,20 +95,20 @@
   :init
   (setq-default frame-background-mode 'dark)
   (add-hook 'window-setup-hook
-	    (lambda ()
-	      (load-theme 'solarized t))))
+      (lambda ()
+        (load-theme 'solarized t))))
 
 (defun ffe/switch-theme-pallete ()
   "Switch between light and dark solarized theme variants"
   (interactive)
-  (let ((next-mode (if (eq frame-background-mode 'dark)
-		       'light 'dark)))
+  (let ((next-mode (if (eq frame-background-mode 'dark))
+           'light 'dark))
     (setq frame-background-mode next-mode)
-    (load-theme 'solarized t)
-    ))
+    (load-theme 'solarized t)))
+    
 
 (general-define-key :keymaps 'ffe-ui-map
-		    "T" 'ffe/switch-theme-pallete)
+        "T" 'ffe/switch-theme-pallete)
 
 ;;
 ;; Treat undo history as a tree
@@ -133,31 +132,31 @@
 
 (use-package counsel
   :diminish (ivy-mode . "")
-  :bind (("C-s" . swiper)
-	 ("C-c C-r" . ivy-resume)
-	 ("<f6>" . ivy-resume)
-	 ("M-x" . counsel-M-x)
-	 ("C-x C-f" . counsel-find-file)
-	 ("<f1> f" . counsel-describe-function)
-	 ("<f1> v" . counsel-describe-variable)
-	 ("<f1> l" . counsel-find-library)
-	 ("<f2> i" . counsel-info-lookup-symbol)
-	 ("<f2> u" . counsel-unicode-char)
-	 ("C-c g" . counsel-git)
-	 ("C-c j" . counsel-git-grep)
-	 ("C-c k" . counsel-ag)
-	 ("C-x l" . counsel-locate))
+  :bind (("C-s" . swiper))
+   ("C-c C-r" . ivy-resume)
+   ("<f6>" . ivy-resume)
+   ("M-x" . counsel-M-x)
+   ("C-x C-f" . counsel-find-file)
+   ("<f1> f" . counsel-describe-function)
+   ("<f1> v" . counsel-describe-variable)
+   ("<f1> l" . counsel-find-library)
+   ("<f2> i" . counsel-info-lookup-symbol)
+   ("<f2> u" . counsel-unicode-char)
+   ("C-c g" . counsel-git)
+   ("C-c j" . counsel-git-grep)
+   ("C-c k" . counsel-ag)
+   ("C-x l" . counsel-locate)
   :general
   (:keymaps 'ffe-spc-map
-	    "SPC" 'counsel-M-x
-	    "/" 'counsel-ag)
+      "SPC" 'counsel-M-x
+      "/" 'counsel-ag)
   (:keymaps 'ffe-buffers-map
-	    "b" 'ivy-switch-buffer)
+      "b" 'ivy-switch-buffer)
   (:keymaps 'ffe-files-map
-	    "f" 'counsel-find-file
-	    "r" 'counsel-recentf)
+      "f" 'counsel-find-file
+      "r" 'counsel-recentf)
   (:keymaps 'ffe-search-map
-	    "s" 'swiper)
+      "s" 'swiper)
   :config
   (ivy-mode 1))
 
@@ -186,8 +185,8 @@
   :diminish (projectile-mode . "")
   :general
   (:keymaps 'ffe-project-map
-	    "f" 'projectile-find-file
-	    "F" 'projectile-find-file-in-known-projects)
+      "f" 'projectile-find-file
+      "F" 'projectile-find-file-in-known-projects)
   :init
   (setq projectile-completion-system 'ivy)
   :config
@@ -210,11 +209,11 @@
     "TAB" 'neotree-enter
     "q" 'neotree-hide)
   (:keymaps 'ffe-files-map
-	    "t" 'neotree-toggle)
+      "t" 'neotree-toggle)
   :init
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow)) ; this will use all-the-icons as icons source
-  (setq neo-smart-open t)
-  )
+  (setq neo-smart-open t))
+  
 
 ;;
 ;; Latest Org-mode from correct repo
@@ -229,12 +228,26 @@
 ;;
 
 (general-define-key :keymaps 'ffe-toggles-map
-		    "n" 'linum-mode)
+        "n" 'linum-mode)
 
 (use-package linum-relative
   :general
   (:keymaps 'ffe-toggles-map
 	    "N" 'linum-relative-toggle))
+
+;;
+;; Lisp editing
+;;
+
+(use-package lispy)
+
+(use-package evil-lispy
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'evil-lispy-mode))
+
+(use-package rainbow-delimiters
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode))
 
 ;;
 ;; Custom
@@ -247,10 +260,25 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (linum-relative all-the-icons projectile company which-key color-theme-solarized general evil use-package))))
+    (rainbow-delimeters evil-lispy linum-relative all-the-icons projectile company which-key color-theme-solarized general evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
